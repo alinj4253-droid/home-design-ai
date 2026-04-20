1144110f2f4c8e78e215fa7d754e8ee181f7c356
alinj4253-droid <alinj4253@gmail.com>
2026-04-20 20:36:17 +0800

    Add files via upload

---
+++ ChuangJu-JavaWeb/chuangju-ai-portal/src/main/java/com/chuangju/chuangju_ai_portal/SpaController.java
@@ -0,0 +1,15 @@
+package com.chuangju.chuangjuaiportal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class SpaController {
+
+
+    @RequestMapping(value = { "/", "/{path:[^\\.]*}", "/**/{path:[^\\.]*}" })
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
---
+++ ChuangJu-JavaWeb/chuangju-ai-portal/src/test/java/com/chuangju/chuangju_ai_portal/ChuangjuAiPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuangju.chuangju_ai_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuangjuAiPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
